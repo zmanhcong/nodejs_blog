7c2cb7f06c436b0dd403ad1bf50119c3b5803d05
nguyenmanhcong <cng.buff@gmail.com>
2022-10-23 17:11:41 +0900

    test prettier

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(morgan('combined'))
 app.engine('hbs', engine({
   extname: '.hbs'     //defautl đuôi nó là handlebars, nhưng mình đặt "hbs" cho nó ngắn gọn
 }));
-    app.set("view engine", 'hbs');
+        app.set("view engine", "hbs");
             app.set('views', path.join(__dirname, 'resources/views'));
 
 // Rroute init
